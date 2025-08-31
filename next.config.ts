99240730e3dece56d8c218a20d4b5079bfdf5d1a
sbjame <surawut.workk@gmail.com>
2025-09-01 01:17:31 +0700

    master

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: false, // ปิด Turbopack
+  },
 };
 
 export default nextConfig;
