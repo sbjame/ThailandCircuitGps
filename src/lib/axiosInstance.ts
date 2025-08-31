38d9f54dec678e2485bcc01ee517468ffb5c56af
sbjame <surawut.workk@gmail.com>
2025-09-01 01:05:38 +0700

    add render html

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { error } from 'console';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://thailandcircuitgpsapi.onrender.com/api',
   timeout: 5000
 });
 
