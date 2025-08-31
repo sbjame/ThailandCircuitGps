b7ae494c7ce449c6014f405fb5c2e29dd41aa236
sbjame <surawut.workk@gmail.com>
2025-09-01 01:06:36 +0700

    a

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { error } from 'console';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://thailandcircuitgpsapi.onrender.com/api',
+  baseURL: process.env.BASE_URL || 'https://thailandcircuitgpsapi.onrender.com/api',
   timeout: 5000
 });
 
