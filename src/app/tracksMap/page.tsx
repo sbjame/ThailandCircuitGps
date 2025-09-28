d36d77c55e34cb33f1e6eaaa6f9e484bfbb35edc
sbjame <surawut.workk@gmail.com>
2025-09-29 01:53:25 +0700

    cache

--- src/app/tracksMap/page.tsx
+++ src/app/tracksMap/page.tsx
@@ -2,6 +2,9 @@ import TrackMapClient from "./TrackMapClient";
 import { fetchTracks } from "@/lib/apiClient";
 import styles from "@/styles/map.module.css";
 
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+
 export default async function TracksMapPage() {
   const tracks = await fetchTracks();
   return (
